6a74f876f0db660cba56b1162a03163d48ac49b8
cr.jrg08 <cr.jrg08@gmail.com>
2025-01-15 01:06:25 +0000

    3rd try to deploy vue

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "/EditMark/",
   plugins: [
     vue({ 
       template: { transformAssetUrls }
